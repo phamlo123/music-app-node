7cfb18ed19a7eb4fb3d39a401e97b03967fc972e
phamlo123 <pham.lo@northeastern.edu>
2023-01-20 22:00:34 -0500

    more

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(cors({
     credentials: true,
-    origin: 'https://rate-my-playlists.netlify.app/'
+    origin: 'https://rate-my-playlists.netlify.app'
 }))
 app.use(session({
     secret: 'should be an environment variable',
